13c8bc3138a82cdf74f6bdb840684f83a565088b
Cole Crandall <cjc5530@g.rit.edu>
2026-09-18 16:37:07 -0400

    Update xor.c
    made all inputs in fgets cause there was an error with combining fgets and fscan

--- xor.c
+++ xor.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-void encrypt(char *message, char *key) {
+void encrypt(char* message, char* key) {
     int key_len = strlen(key);
     for (int i = 0; message[i] != '\0'; i++) {
         message[i] ^= key[i % key_len];
     }
 }
 
-void decrypt(char *data, char *key) {
+void decrypt(char* data, char* key) {
     int key_len = strlen(key);
     for (int i = 0; data[i] != '\0'; i++) {
         data[i] ^= key[i % key_len];
@@ -16,48 +16,65 @@ void decrypt(char *data, char *key) {
 }
 
 int main() {
-    char choice[10], message[256], filename[256], key[256];
+    char choice[10];
+    char message[256];
+    char filename[256];
+    char key[256];
     printf("Do you want to encrypt or decrypt? ");
-    scanf("%s", choice);
-    
+	fgets(choice, sizeof(choice), stdin);
+
+    choice[strcspn(choice, "\n")] = '\0';
     if (strcmp(choice, "encrypt") == 0) {
-        printf("Enter the string to encrypt: ");
+        printf("Enter the string to encrypt: \n");
         fgets(message, sizeof(message), stdin);
         message[strcspn(message, "\n")] = '\0'; // Remove newline character
+        //printf("messsage = %s\n", message);
+
         printf("Enter the filename to save encrypted data: ");
-        scanf("%s", filename);
+        fgets(filename, sizeof(filename), stdin);
+        filename[strcspn(filename, "\n")] = '\0';
+        //printf("filename = %s\n", filename);
+
         printf("Enter the key for encryption: ");
-        scanf("%s", key);
-        
-        FILE *file = fopen(filename, "w");
+        fgets(key, sizeof(key), stdin);
+        key[strcspn(key, "\n")] = '\0';
+        //printf("key = %s\n", key);
+
+        FILE* file = fopen(filename, "w");
         if (file == NULL) {
             perror("Error opening file");
             return 1;
         }
-        
+
         encrypt(message, key);
         fprintf(file, "%s", message);
         fclose(file);
-    } else if (strcmp(choice, "decrypt") == 0) {
+    }
+    else if (strcmp(choice, "decrypt") == 0) {
         printf("Enter the filename to read encrypted data: ");
-        scanf("%s", filename);
+        fgets(filename, sizeof(filename), stdin);
+        filename[strcspn(filename, "\n")] = '\0';
+
         printf("Enter the key for decryption: ");
-        scanf("%s", key);
-        
-        FILE *file = fopen(filename, "r");
+        fgets(key, sizeof(key), stdin);
+        key[strcspn(key, "\n")] = '\0';
+
+
+        FILE* file = fopen(filename, "r");
         if (file == NULL) {
             perror("Error opening file");
             return 1;
         }
-        
+
         fgets(message, sizeof(message), file);
         fclose(file);
-        
+
         decrypt(message, key);
         printf("Decrypted message: %s\n", message);
-    } else {
+    }
+    else {
         printf("Invalid choice.\n");
     }
-    
+
     return 0;
 }
